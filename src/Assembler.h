b09b822c2aff19c88fb7946de73aa16f5105b07d
Alexey-Pfeifer <alexeypeterpfeifer@gmail.com>
2024-11-14 19:37:30 +0100

    Assembler.h hinzugefügt

--- src/Assembler.h
+++ src/Assembler.h
@@ -18,21 +18,41 @@ class Assembler
 
 	/**
 	 * Creates an overlap graph.
+	 * TODO-done
 	 */
-	Assembler(const std::vector<Assembler::Seq>& sequences); // TODO
+	Assembler::Assembler(const std::vector<Assembler::Seq>& sequences) {
+		for (const auto& seq : sequences) {
+			graph_.addNode(seq);
+		}
+		buildEdges();
+	}	
 
 	/**
 	 * Getter for graph_
+	 * TODO-done
 	 */
-	OGraph& getGraph(); // TODO
+	OGraph& getGraph(){
+		return graph_;
+	}
 
   private:
-
 	/**
 	 * Initializes the appropriate Edges between the given nodes to create
 	 * the Overlay-Graph.
+	 * TODO-done
 	 */
-	void buildEdges(); // TODO
+	void Assembler::buildEdges() {
+    for (auto n1 = graph_.beginNodes(); n1 != graph_.endNodes(); ++n1) {
+        for (auto n2 = graph_.beginNodes(); n2 != graph_.endNodes(); ++n2) {
+            // Berechne die Überlappung zwischen den Sequenzen
+            size_t overlap_len = n1->label.overlap(n2->label);
+
+            if (overlap_len > 0) {
+                graph_.addEdge(&(*n1), &(*n2), overlap_len);
+            }
+        }
+    }
+}
 
 	OGraph graph_;
 
